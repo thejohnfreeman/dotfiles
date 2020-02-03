16a931e5073cba4632b40a16ce628059ee6e8a0e
John Freeman <jfreeman08@gmail.com>
2020-02-02 19:24:04 -0600

    Streamline setup

--- README.md
+++ README.md
@@ -1,5 +1,32 @@
 ## Installation
 
+- Add [u2f rules](https://docs.01.org/clearlinux/latest/tutorials/yubikey-u2f.html):
+
+```
+sudo apt install curl git
+curl -O https://raw.githubusercontent.com/Yubico/libu2f-host/master/70-u2f.rules
+sudo mv 70-u2f.rules /etc/udev/rules.d/
+sudo udevadm control --reload-rules && sudo udevadm trigger
+```
+
+- Install the [LastPass addon for Firefox](https://addons.mozilla.org/en-US/firefox/addon/lastpass-password-manager/) and log in.
+
+- Create an SSH key for this machine. Never copy the private half anywhere.
+
+```
+ssh-keygen -t rsa -b 4096 -C "$(whoami)@$(hostname)"
+eval "$(ssh-agent -s)"
+ssh-add ~/.ssh/id_rsa
+```
+
+- Log in to GitHub and [add your SSH key](https://github.com/settings/keys):
+
+```
+xclip -i -sel clip < ~/.ssh/id_rsa.pub
+```
+
+- Check out your dotfiles.
+
 ```
 export GIT_DIR=$HOME/.files
 git init --bare
@@ -7,6 +34,13 @@ git config --local status.showUntrackedFiles no
 git remote add origin git@github.com:thejohnfreeman/dotfiles.git
 git fetch
 git --work-tree=$HOME checkout --force ubuntu
+```
+
+- Settle in and script the rest.
+
+```
+source .bash_aliases
+./README.sh
 ```
 
 - [Base16 Gnome Terminal](https://github.com/aaron-williamson/base16-gnome-terminal)
--- README.sh
+++ README.sh
@@ -1,28 +1,18 @@
-# This script only meant for Ubuntu 18.04 LTS.
-
 # Install packages
-# See http://askubuntu.com/a/548005/538768
-sudo add-apt-repository ppa:ubuntu-mozilla-daily/firefox-aurora
-# Unofficial Gradle PPA
-sudo add-apt-repository ppa:cwchien/gradle
-# https://github.com/nodesource/distributions/blob/master/README.md
-curl -sL https://deb.nodesource.com/setup_11.x | sudo -E bash -
+# https://github.com/nodesource/distributions/blob/master/README.md#installation-instructions
+curl -sL https://deb.nodesource.com/setup_13.x | sudo -E bash -
 # https://yarnpkg.com/lang/en/docs/install/#debian-stable
 curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
 echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
-# https://code.visualstudio.com/docs/setup/linux
-curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
-sudo install -o root -g root -m 644 microsoft.gpg /etc/apt/trusted.gpg.d/
-sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
+# KitWare makes CMake.
 # https://apt.kitware.com/
 wget -O - https://apt.kitware.com/keys/kitware-archive-latest.asc 2>/dev/null | sudo apt-key add -
 sudo apt-add-repository 'deb https://apt.kitware.com/ubuntu/ bionic main'
 
 sudo apt install apt-transport-https
 sudo apt update
-sudo apt install code curl entr tmux git nodejs vim-gtk xclip yarn tree \
-  silversearcher-ag stack openjdk-8-jdk gradle jq libssl-dev cmake firefox \
-  zlib-dev
+sudo apt install curl entr tmux git nodejs neovim yarn tree \
+  silversearcher-ag jq libssl-dev cmake zlib-dev
 
 sudo apt install clang-8 clang-format-8
 sudo update-alternatives \
@@ -41,31 +31,10 @@ sudo apt-get install make build-essential libssl-dev zlib1g-dev libbz2-dev \
 pyenv install 3.7.1
 pyenv global 3.7.1
 
-# https://docs.haskellstack.org/en/stable/install_and_upgrade/
-curl -sSL https://get.haskellstack.org/ | sh
-
-# Set up new ssh key for this host
-ssh-keygen -t rsa -b 4096 -C "$(whoami)@$(hostname)"
-eval "$(ssh-agent -s)"
-ssh-add ~/.ssh/id_rsa
-# Copy for GitHub.
-xclip -i ~/.ssh/id_rsa.pub
-
 # Install vim plugins
 curl --fail --location --output ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 vim +PlugInstall +qa
-
-# Install GHC and packages
-stack setup
-stack install ghc-mod hlint
-stack install --resolver nightly hindent
-
-# Enable support for Yubikey in Firefox
-# https://docs.01.org/clearlinux/latest/tutorials/yubikey-u2f.html
-curl -O https://raw.githubusercontent.com/Yubico/libu2f-host/master/70-u2f.rules
-sudo mv 70-u2f.rules /etc/udev/rules.d/
-sudo udevadm control --reload-rules && sudo udevadm trigger
 
 sudo apt install libcairo2-dev libgirepository1.0-dev libpgpme-dev
 pip install pycairo

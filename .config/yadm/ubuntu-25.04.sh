6caf6b2e2eaec4ee749737d779087a5df3b12de3
John Freeman <jfreeman08@gmail.com>
2025-04-22 10:57:52 -0500

    Add bootstrap for Ubuntu 25.04

---
+++ .config/yadm/ubuntu-25.04.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+set -o errexit
+set -o nounset
+set -o pipefail
+
+# Parameters.
+CLANG_VERSION='19'
+NODE_VERSION='23'
+PYTHON_VERSION='3.12'
+
+source libubuntu.sh
+
+# Install packages.
+# KitWare makes CMake, but their PPA only publishes for LTS releases.
+sudo add-apt-repository --yes ppa:neovim-ppa/unstable
+sudo apt update
+sudo apt install --yes apt-transport-https
+packages=(
+  cmake
+  curl
+  docker.io
+  entr
+  git
+  jq
+  kitty
+  libssl-dev
+  neovim
+  ninja-build
+  pipx
+  ripgrep
+  tmux
+  tree
+  tree-sitter-cli
+  xclip
+)
+sudo apt install --yes ${packages[@]}
+
+install-node ${NODE_VERSION}
+install-clang ${CLANG_VERSION}
+install-pyenv
+install-python ${PYTHON_VERSION}
+pipx install poetry conan
+install-vim-plugins
+generate-ssh-key
+configure-yadm
+
+# Restart.
